f12d327b5a36a19956cf40f90e42005f5d0eea1c
Mark Talbot <markjtalbot@gmail.com>
2017-05-15 23:29:05 -0400

    Adding anagram solution

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -4,31 +4,42 @@ class Anagram {
 
     constructor(word) {
         this.word = word;
-        this.wordsArray = [];
     }
 
-    matches(candidates) {
-        this.getWordsOfEqualLength(candidates);
-        this.getAnagrams(this.wordsArray);
-        console.log(this.wordsArray);
-
-        return this.wordsArray;
-    }
-
-    getWordsOfEqualLength(candidates) {
-        let words = candidates.filter((candidate) => candidate.length === this.word.length);
-        this.wordsArray = [...this.wordsArray, ...words];
-
-        return this;
+    matches(...candidates) {
+        return this.getAnagrams(candidates);
     }
 
     getAnagrams(candidates) {
-        let words = candidates.filter((candidate) => {
-            let letterArray = candidate.split('');
-            return letterArray.every((letter) => this.word.includes(letter));
-        });
+        let words = this.transformInput(candidates)
+            .filter((candidate) => !this.isOwnAnagram(candidate, this.word))
+            .filter((candidate) => this.isSameLength(candidate, this.word))
+            .filter((candidate) => {
+                let wordArray      = this.word.split('').sort((a,b) => a.toLowerCase() > b.toLowerCase());
+                let candidateArray = candidate.split('').sort((a,b) => a.toLowerCase() > b.toLowerCase());
+                return wordArray.every((value, index) => {
+                    return value.match(new RegExp(candidateArray[index], 'i'));
+                });
+            }
+        );
+        return words;
+    }
 
-        return this;
+    transformInput(candidates) {
+        let input = candidates.length === 1 ? candidates[0] : candidates;
+
+        if (typeof input === 'string') {
+            return [input];
+        }
+        return input;
+    }
+
+    isSameLength(str1, str2) {
+        return str1.length === str2.length;
+    }
+
+    isOwnAnagram(str1, str2) {
+        return !!str1.match(new RegExp(str2, 'i'));
     }
 }
 
